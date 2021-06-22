3f7804c4c8e38fee5641e07f31dcdedf949e0597
MorPahima <adika@adikas-MacBook-Pro-3.local>
2021-06-22 13:19:51 +0300

    changed home path cotext for test

--- cmd/handlers/noam_path.go
+++ cmd/handlers/noam_path.go
@@ -7,6 +7,6 @@ import (
 
 func Noam() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Do you like it Baby? :D")
+		ctx.String(http.StatusOK, "Just test to deploy with github")
 	}
 }
